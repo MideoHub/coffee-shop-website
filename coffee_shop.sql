d1d235f06a1e1a862acd9f57e77e66d1bc0a4883
khairy-nio <Zxcvbnm2005asd2005@icloud.com>
2025-05-25 10:05:47 +0300

    Edit coffee_shop.sql

--- coffee_shop.sql
+++ coffee_shop.sql
@@ -21,6 +21,7 @@ CREATE TABLE `products` (
   `image` varchar(255) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `nutrition` varchar(255) NOT NULL,
+  `category` enum('Menu','Products') NOT NULL DEFAULT 'Products',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -54,41 +55,50 @@ CREATE TABLE `bookings` (
   CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Sample data: users
+-- Dumping data for `users`
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `role`) VALUES
 (1, 'admin', 'Admin@gmail.com', '$2y$10$FcmFN3TSb9ZPAhrzUJMoxOQBWaN3KV9RAnF/cOTLQfm11uNXpj6OG', '2025-05-21 00:47:42', 'admin'),
-(2, 'mohamdd', 'asd@gmial.com', '$2y$10$FcmFN3TSb9ZPAhrzUJMoxOQBWaN3KV9RAnF/cOTLQfm11uNXpj6OG', '2025-05-21 02:34:55', 'user');
+(2, 'mohamdd', 'asd@gmail.com', '$2y$10$FcmFN3TSb9ZPAhrzUJMoxOQBWaN3KV9RAnF/cOTLQfm11uNXpj6OG', '2025-05-21 02:34:55', 'user'),
+(3, 'mmm', 'as@gmail.com', '$2y$10$FcmFN3TSb9ZPAhrzUJMoxOQBWaN3KV9RAnF/cOTLQfm11uNXpj6OG', '2025-05-21 20:06:25', 'user'),
+(4, 'a7med', 'a@gmail.com', '$2y$10$2rkEM0uDws3fPbiPXvBVEu46PSn2GmsEfVW9iQaWxi/CN0fv4gCYO', '2025-05-22 00:38:51', 'user'),
+(5, 'marwan', 'm@gmail.com', '$2y$10$Cx0AdOqun4H0e9FkOs7T8e5y.lD7JVdo3Mb1sIYKOZfd.mXUJfPtW', '2025-05-22 09:16:16', 'user'),
+(6, 'eng aya ', 'aed@gmail.com', '$2y$10$kVjyhfoWWgTTpXZNP7Ps8.dEBeDfwhqXsoWP17XEHPC2xnUop5R8C', '2025-05-22 10:37:01', 'user');
 
--- Sample data: products
-INSERT INTO `products` (`id`, `name`, `description`, `image`, `price`, `nutrition`) VALUES
-(1, 'Caffè Americano', 'A rich, full-bodied coffee with a hint of caramel.', '../assets/images/caffe-americano.jpg', 2.10, 'Serving Size: 16 fl oz|Calories: 15|Calories from Fat: 0'),
-(2, 'Caffè Misto', 'A one-to-one combination of fresh-brewed coffee and steamed milk.', '../assets/images/caffe-misto.jpg', 2.60, 'Serving Size: 16 fl oz|Calories: 110|Calories from Fat: 35'),
-(3, 'Espresso', 'Rich and full-bodied with a hint of caramel.', '../assets/images/espresso.jpg', 2.79, 'Serving Size: 1.5 fl oz|Calories: 5|Calories from Fat: 0'),
-(4, 'Espresso Macchiato', 'A rich, full-bodied espresso with a dash of steamed milk.', '../assets/images/espresso-macchiato.jpg', 2.05, 'Serving Size: 1.5 fl oz|Calories: 15|Calories from Fat: 5'),
-(5, 'Dark Roast Coffee', 'A full-bodied coffee with bold, robust flavors.', '../assets/images/caffe-americano.jpg', 2.20, 'Serving Size: 16 fl oz|Calories: 5|Calories from Fat: 0'),
-(6, 'Flat White', 'Smooth ristretto shots of espresso with velvety steamed milk.', '../assets/images/flat-white.jpg', 2.80, 'Serving Size: 12 fl oz|Calories: 170|Calories from Fat: 90'),
-(7, 'Decaf Roast', 'A decaffeinated coffee with a smooth, rich flavor.', '../assets/images/caffe-misto.jpg', 2.25, 'Serving Size: 16 fl oz|Calories: 5|Calories from Fat: 0'),
-(8, 'Cappuccino', 'A perfect balance of espresso, steamed milk, and foam.', '../assets/images/cappuccino.jpg', 2.59, 'Serving Size: 12 fl oz|Calories: 120|Calories from Fat: 50'),
-(9, 'Blonde Roast', 'A light-bodied coffee with a sweet, citrusy flavor.', '../assets/images/caffe-americano.jpg', 2.59, 'Serving Size: 16 fl oz|Calories: 5|Calories from Fat: 0'),
-(10, 'aaa', 'free', 'E:\\Xampp\\htdocs\\coffee-shop-website\\assets\\images\\p6.jpeg', 2.50, 'aaa');
+-- Dumping data for `products`
+INSERT INTO `products` (`id`, `name`, `description`, `image`, `price`, `nutrition`, `category`) VALUES
+(2, 'Caffè Misto', 'A one-to-one combination of fresh-brewed coffee and steamed milk.', '../assets/images/caffe-misto.jpg', 2.60, 'Serving Size: 16 fl oz|Calories: 110|Calories from Fat: 35', 'Products'),
+(3, 'Espresso', 'Rich and full-bodied with a hint of caramel.', '../assets/images/espresso.jpg', 2.79, 'Serving Size: 1.5 fl oz|Calories: 5|Calories from Fat: 0', 'Products'),
+(4, 'Espresso Macchiato', 'A rich, full-bodied espresso with a dash of steamed milk.', '../assets/images/espresso-macchiato.jpg', 2.05, 'Serving Size: 1.5 fl oz|Calories: 15|Calories from Fat: 5', 'Products'),
+(5, 'Dark Roast Coffee', 'A full-bodied coffee with bold, robust flavors.', '../assets/images/caffe-americano.jpg', 2.20, 'Serving Size: 16 fl oz|Calories: 5|Calories from Fat: 0', 'Products'),
+(6, 'Flat White', 'Smooth ristretto shots of espresso with velvety steamed milk.', '../assets/images/flat-white.jpg', 2.80, 'Serving Size: 12 fl oz|Calories: 170|Calories from Fat: 90', 'Products'),
+(7, 'Decaf Roast', 'A decaffeinated coffee with a smooth, rich flavor.', '../assets/images/caffe-misto.jpg', 2.25, 'Serving Size: 16 fl oz|Calories: 5|Calories from Fat: 0', 'Products'),
+(8, 'Cappuccino', 'A perfect balance of espresso, steamed milk, and foam.', '../assets/images/cappuccino.jpg', 2.59, 'Serving Size: 12 fl oz|Calories: 120|Calories from Fat: 50', 'Products'),
+(15, 'Free ice cream', 'ice by ice', '../assets/images/main.jpeg', 1.00, '300CA', 'Products');
 
--- Sample data: cart_items
+-- Dumping data for `cart_items`
 INSERT INTO `cart_items` (`id`, `user_id`, `session_id`, `product_name`, `product_image`, `price`, `quantity`, `created_at`) VALUES
-(1, NULL, 't2p7u8iqipbc6g9ime1p3p1b4i', 'Caffè Misto', '../assets/images/caffe-misto.jpg', 2.60, 1, '2025-05-21 00:29:52'),
-(2, NULL, 't2p7u8iqipbc6g9ime1p3p1b4i', 'Caffè Americano', '../assets/images/caffe-americano.jpg', 2.10, 1, '2025-05-21 00:30:23'),
-(3, 1, 't2p7u8iqipbc6g9ime1p3p1b4i', 'Caffè Misto', '../assets/images/caffe-misto.jpg', 2.60, 1, '2025-05-21 00:54:23'),
-(4, 1, 't2p7u8iqipbc6g9ime1p3p1b4i', 'Caffè Misto', '../assets/images/p2.jpeg', 2.60, 1, '2025-05-21 00:54:36'),
-(9, 2, 'gn94ivt3s3rge9u1v6704lqke6', 'Caffè Americano', '../assets/images/caffe-americano.jpg', 2.10, 1, '2025-05-21 03:04:29'),
-(10, 2, 'gn94ivt3s3rge9u1v6704lqke6', 'aaa', 'E:\\Xampp\\htdocs\\coffee-shop-website\\assets\\images\\p6.jpeg', 2.50, 1, '2025-05-21 03:07:58'),
-(11, 2, 'gn94ivt3s3rge9u1v6704lqke6', 'Dark Roast Coffee', '../assets/images/p5.jpeg', 2.20, 1, '2025-05-21 03:29:50');
+(10, NULL, 'ui8f67k3gvc47sitqcimrhruvo', 'Caffè Misto', '../assets/images/p2.jpeg', 2.60, 1, '2025-05-21 20:01:07'),
+(11, NULL, 'ui8f67k3gvc47sitqcimrhruvo', 'Caffè Misto', '../assets/images/caffe-misto.jpg', 2.60, 1, '2025-05-21 20:06:00'),
+(12, 3, 'ui8f67k3gvc47sitqcimrhruvo', 'Caffè Misto', '../assets/images/caffe-misto.jpg', 2.60, 1, '2025-05-21 20:15:02'),
+(20, 2, 'dio9isi0ee73atj3ibqjmrfv56', 'Espresso', '../assets/images/espresso.jpg', 2.79, 1, '2025-05-22 10:38:13'),
+(21, 2, 'dio9isi0ee73atj3ibqjmrfv56', 'Espresso', '../assets/images/p3.jpeg', 2.79, 1, '2025-05-22 10:38:18');
 
--- Sample data: bookings
+-- Dumping data for `bookings`
 INSERT INTO `bookings` (`id`, `user_id`, `name`, `email`, `booking_date`, `booking_time`, `persons`, `created_at`) VALUES
-(1, NULL, '‪mohmed hmdy‬‏ن', 'mohmedhmdy95@gmail.com', '2025-05-26', '20:47:00', 3, '2025-05-21 00:43:36'),
-(2, NULL, '‪mohmed hmdy‬‏ن', 'mohmedhmdy95@gmail.com', '2025-05-26', '20:47:00', 3, '2025-05-21 00:43:58'),
-(3, NULL, 'aaaaaaaaaaaa‬‏ن', 'mohmedhmdy95@gmail.com', '2025-05-24', '20:47:00', 1, '2025-05-21 00:44:52'),
-(4, 2, 'mohamdd', 'mohmedhmdy95@gmail.com', '2025-05-08', '08:48:00', 4, '2025-05-21 02:49:04'),
-(5, 2, 'mohamdd', 'mohmedhmdy95@gmail.com', '2025-05-14', '05:53:00', 3, '2025-05-21 02:51:54'),
-(6, 2, 'mohamdd', 'mohmedhmdy95@gmail.com', '2025-05-30', '05:53:00', 3, '2025-05-21 02:52:02'),
-(7, 2, 'mohamdd', 'mohmedhmdy95@gmail.com', '2025-05-09', '05:53:00', 3, '2025-05-21 02:53:22'),
-(8, 2, 'mohamdd', 'mohmedhmdy95@gmail.com', '2025-06-18', '05:53:00', 3, '2025-05-21 02:53:31');
+(15, 2, 'mohamdd', 'mohmedhmdy95@gmail.com', '2025-05-31', '01:25:00', 3, '2025-05-21 06:25:51'),
+(16, 2, 'admin', 'mohmedhmdy95@gmail.com', '2025-05-31', '09:34:00', 2, '2025-05-21 06:31:37'),
+(17, 2, 'admin', 'mohmedhmdy95@gmail.com', '2025-05-31', '09:34:00', 2, '2025-05-21 06:31:37'),
+(18, 2, 'admin', 'mohmedhmdy95@gmail.com', '2025-05-28', '11:34:00', 2, '2025-05-21 06:34:12'),
+(19, 2, 'admin', 'mohmedhmdy95@gmail.com', '2025-05-28', '11:34:00', 2, '2025-05-21 06:34:12'),
+(20, 2, 'mohamdd', 'mohmedhmdy95@gmail.com', '2025-05-31', '01:36:00', 2, '2025-05-21 06:36:09'),
+(27, 4, 'ccc', 'asd@gmail.com', '2025-05-30', '08:00:00', 3, '2025-05-22 01:01:00'),
+(28, 4, 'zz', 'mohmedhmdy95@gmail.com', '2025-05-31', '08:03:00', 3, '2025-05-22 01:03:29'),
+(29, 4, 'zz', 'mohmedhmdy95@gmail.com', '2025-05-31', '08:03:00', 3, '2025-05-22 01:03:29'),
+(30, 4, 'ccc', 'mohmedhmdy95@gmail.com', '2025-05-24', '08:06:00', 2, '2025-05-22 01:06:16'),
+(31, 4, 'ccc', 'mohmedhmdy95@gmail.com', '2025-05-24', '08:06:00', 2, '2025-05-22 01:06:16'),
+(32, 4, 'admin', 'mohmedhmdy95@gmail.com', '2025-05-24', '08:10:00', 3, '2025-05-22 01:10:21'),
+(33, 4, 'admin', 'mohmedhmdy95@gmail.com', '2025-05-24', '08:10:00', 3, '2025-05-22 01:10:21'),
+(34, 2, 'marwan', 'mohmedhmdy95@gmail.com', '2025-05-31', '16:14:00', 3, '2025-05-22 09:15:02'),
+(35, 2, 'marwan', 'mohmedhmdy95@gmail.com', '2025-05-31', '16:14:00', 3, '2025-05-22 09:15:02'),
+(36, 2, 'mawaran', 'm@gmai.com', '2025-05-31', '16:42:00', 4, '2025-05-22 10:40:44'),
+(37, 2, 'mawaran', 'm@gmai.com', '2025-05-31', '16:42:00', 4, '2025-05-22 10:40:45');
